bad3ad66e83fd98bb24079fcd8f13b361290c6c5
Liselle Logan <liselle.logan@gmail.com>
2020-09-22 17:21:31 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,15 @@
-# Your code goes here!
+class Dog
+
+  def name=(dog_nane)
+    @this_dogs_name = dog_nane
+  end
+
+  def name
+    @this_dogs_name
+  end
+
+  def bark
+    puts "woof!"
+  end
+
+end
